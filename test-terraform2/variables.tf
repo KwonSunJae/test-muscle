f0cec5926b6a0c0f8b1966caed2c1211ae4786f4
KwonSunJae <ksun4131@gmail.com>
2024-08-25 18:12:44 +0900

    Enroll by muscle process

---
+++ test-terraform2/variables.tf
@@ -0,0 +1,14 @@
+variable "USER_NAME"{
+    type    = string
+}
+variable "USER_PW"{
+    type    = string
+}
+
+variable "TENANT_NAME"{
+    type    = string
+}
+
+variable "CTRL_URL"{
+    type    = string
+}
